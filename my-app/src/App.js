c6db31f1a4dc4d9f728037fcadb34b9c79039fc0
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-12-11 22:24:41 +0300

    dd

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,18 +4,19 @@ import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import Regestier from "./components/Regestier";
 import Home from "./components/Home";
-
+import Post from './components/Post'
+import Posts from './components/posts'
 
 function App() {
   return (
     <>
-     {/* <Navbar/> */}
-      <Routes>
-        <Route exact path="/login" element={<Login/>} />
-        <Route exact path="/Navbar" element={<Navbar/>} />
-        <Route path="/Regestier" element={<Regestier />} />
-        <Route path="/" element={<Home />} />
-       
+     <Navbar/>
+     <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path='/login' element={<Login />}/>
+        <Route exact path='/Regestier' element={<Regestier />}/>
+        <Route exact path="/post/:id" element={<Post/>}/>
+        <Route exact path="/posts" element={<Posts/>}/>
       </Routes>
       </>
   );
--- my-app/src/components/Login/index.js
+++ my-app/src/components/Login/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import './style.css'
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
@@ -9,10 +10,7 @@ const Login = () => {
   const login = async (e) => {
     try {
       e.preventDefault();
-      // console.log(result);
-      console.log(e.target.email.value)
       const result = await axios.post(`${BASE_URL}/login`, {
-        
         email: e.target.email.value,
         password: e.target.password.value,
       });
@@ -20,7 +18,8 @@ const Login = () => {
         setErr(result.data.err);
         // localStorage.setItem("role", result.data.result.role.role);
       } else if (result.data.success) {
-        navigate("/");
+        console.log('yes');
+        // navigate("/posts");
       }
     } catch (error) {
       console.log(error);
@@ -28,30 +27,48 @@ const Login = () => {
   };
 
   return (
+    // <section className="section-login vvv">
+    //         <div className="login-box">
+    //             <form onSubmit={ckeck} className={"form"}>
+    //                 <div className="input-field">
+    //                     <p>Email</p>
+    //                     <input type="text" name="email" placeholder="example@gmail.com"
+    //                            onChange={(e) => setEmail(e.target.value)}/>
+    //                 </div>
+    //                 <div className="input-field">
+    //                     <p>password</p>
+    //                     <input type="password" name="password" placeholder="ex.12345"
+    //                            onChange={(e) => setPassword(e.target.value)}/>
+    //                 </div>
+    //                 <input type="submit" value="Login" className={"btn"}/>
+    //                 <p><Link to="/Regestier" className={"register"}>Don't have an account ?</Link></p>
+    //             </form>
+    //         </div>
+    //     </section>
     <div className="home">
-      <div className="formm">
-        <h1>Login</h1>
+    <div className="formm">
+      <h1>Login</h1>
 
-        <form onSubmit={login}>
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email" />
-          <label htmlFor="password">Password:</label>
-          <input type="password" name="password" />
-          <button type="submit">Login</button>
-        </form>
-        <p>{err}</p>
-        <p className="forgot" onClick={() => {
-            navigate("/forgot");
-          }}>Forget Password?</p>
-        <button
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          Back
-        </button>
-      </div>
+      <form onSubmit={login}>
+        <label htmlFor="email">Email:</label>
+        <input type="email" name="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" />
+        <button type="submit">Login</button>
+      </form>
+      <p>{err}</p>
+      <p className="forgot" onClick={() => {
+          navigate("/forgot");
+        }}>Forget Password?</p>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Back
+      </button>
     </div>
+  </div>
   );
 };
 
